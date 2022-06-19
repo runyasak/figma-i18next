84efa8d7b615cd511d974319650c1db06c4c5d95
Apirak Panatkool <apirakb@gmail.com>
2022-06-19 23:34:38 +0700

    Load i18n from figma

--- src/updateText.ts
+++ src/updateText.ts
@@ -1,29 +1,44 @@
 import { setRelaunchButton } from "@create-figma-plugin/utilities";
-import i18next from 'i18next';
+import i18next from "i18next";
+
+function updateAllTextProperty() {
+  const tEn = figma.currentPage.findAll((node) => /##t.en/.test(node.name));
+  const tEnJson = (<TextNode>tEn[0]).characters;
+  const tEnJsonObject = JSON.parse(`{ ${tEnJson} }`);
+
+  i18next.init({
+    lng: "en", // if you're using a language detector, do not define the lng option
+    debug: true,
+    resources: {
+      en: {
+        translation: tEnJsonObject,
+      },
+    },
+  });
+
+  const textNodes = figma.currentPage.findAll((node) => /#|_#/.test(node.name));
+
+  textNodes.forEach((textNode) => {
+    console.log(textNode.name);
+  });
+
+  // await Promise.all(
+  // console.log("bank");
+  // );
+}
 
 export default function () {
   setRelaunchButton(figma.currentPage, "figma-i18next", {
     description: "🔍 Update text",
   });
 
-  const tEN = figma.currentPage.findAll((node)=> /##t.en/.test(node.name));
-  const tEnResource = (<TextNode>tEN[0]).characters;
-  // const tEnResourceObject = JSON.parse(tEnResource);
+  updateAllTextProperty();
 
-  // console.log('tEn', tEnResourceObject);
-
-  // i18next.init({
-  //   compatibilityJSON: 'v3',
-  //   lng: 'en',
-  //   debug: true,
-  //   resources: {
-  //     en: tEnResourceObject
-  //   }
+  // updateAllTextProperty().then(() => {
+  //   figma.closePlugin("Updated 🎉");
   // });
-
-  // console.log('i18n', i18next);
-
-  // console.log("i18n", i18next.t('key'));
 
   figma.closePlugin("Updated 🎉");
 }
+
+export { updateAllTextProperty };
