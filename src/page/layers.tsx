25f49ae481d6e6bb830c7fd4b65490f17e5f5c0d
Apirak Panatkool <apirakb@gmail.com>
2022-11-25 06:15:49 +0700

    Format tsx with prettier

--- src/page/layers.tsx
+++ src/page/layers.tsx
@@ -1,39 +1,54 @@
-import { h, JSX } from 'preact';
-import { useCallback } from 'preact/hooks';
-import { emit } from '@create-figma-plugin/utilities';
-import { CloseHandler } from '../types'
-import { Container, VerticalSpace, Text, Columns, Button} from '@create-figma-plugin/ui';
-import { Textbox, IconLayerFrame16, IconStyles32, IconComponent32, IconButton } from '@create-figma-plugin/ui';
-import { useState } from 'preact/hooks';
+import { h, JSX } from "preact";
+import { useCallback } from "preact/hooks";
+import { emit } from "@create-figma-plugin/utilities";
+import { CloseHandler } from "../types";
+import {
+  Container,
+  VerticalSpace,
+  Text,
+  Columns,
+  Button,
+} from "@create-figma-plugin/ui";
+import {
+  Textbox,
+  IconLayerFrame16,
+  IconStyles32,
+  IconComponent32,
+  IconLayerText16,
+  IconButton,
+} from "@create-figma-plugin/ui";
+import { useState } from "preact/hooks";
 
 const Layers = () => {
-  const [value, setValue] = useState<string>('Text')
+  const [value, setValue] = useState<string>("Text");
   const [textLayers, setTextLayers] = useState([
-    { name: 'TT', value: 'Text 1' },
-    { name: 'BB', value: 'Text 2' }
-  ])
+    { name: "TT", value: "Text 1" },
+    { name: "BB", value: "Text 2" },
+  ]);
 
   const handleCreateRectanglesButtonClick = useCallback(() => {
-    console.log("in Update Page");h
-  }, [])
+    console.log("in Update Page");
+    h;
+  }, []);
 
-  const handleCloseButtonClick = useCallback(
-    function () {
-      // console.log("in Update All");
-      emit<CloseHandler>('CLOSE')
-    }, []
-  )
+  const handleCloseButtonClick = useCallback(function () {
+    // console.log("in Update All");
+    emit<CloseHandler>("CLOSE");
+  }, []);
 
   function handleInput(index: number, e: JSX.TargetedEvent<HTMLInputElement>) {
-    const newValue = e.currentTarget.value
-    console.log(newValue)
-    setValue(newValue)
+    const newValue = e.currentTarget.value;
+    console.log(newValue);
+    setValue(newValue);
   }
 
   // const handleCreateComponent = (event: JSX.MouseEventHandler<HTMLButtonElement>) => {
-  const handleCreateComponent = (index: number, e: JSX.TargetedMouseEvent<HTMLButtonElement>) => {
-    console.log("create Component")
-  }
+  const handleCreateComponent = (
+    index: number,
+    e: JSX.TargetedMouseEvent<HTMLButtonElement>
+  ) => {
+    console.log("create Component");
+  };
 
   // const handleFormChange = (index: number, e: JSX.TargetedEvent<HTMLInputElement>) => {
   //   let data = [...inputFields];
@@ -46,37 +61,44 @@ const Layers = () => {
   // }
 
   return (
-     <Container space="medium">
-       <VerticalSpace space="large" />
-       <Text muted>Layers</Text>
-       <VerticalSpace space="small" />
-       <form>
-         {textLayers.map((textLayer, index) => {
-          return (
-            <div key={index}>
-              <Columns space="extraSmall">
-                <Textbox icon={<IconLayerFrame16 />} value={textLayer.name} onChange={e => handleInput(index, e)}/>
-                <Textbox value={textLayer.value} onChange={e => handleInput(index, e)}/>
-                <IconButton onClick={e => handleCreateComponent(index, e)}>
-                  <IconComponent32 />
-                </IconButton>
-              </Columns>
-            </div>
-          )
-         })}
-       </form>
-       <VerticalSpace space="small" />
-       <Columns space="extraSmall">
-         <Button fullWidth onClick={handleCreateRectanglesButtonClick}>
-           Create
-         </Button>
-         <Button fullWidth onClick={handleCloseButtonClick} secondary>
-           Close
-         </Button>
-       </Columns>
-       <VerticalSpace space="small" />
-     </Container>
-  )
-}
+    <Container space='medium'>
+      <VerticalSpace space='large' />
+      <Text muted>Layers</Text>
+      <VerticalSpace space='small' />
+      {/* <form> */}
+      {textLayers.map((textLayer, index) => {
+        return (
+          <div key={index}>
+            <Columns space='extraSmall'>
+              <Textbox
+                icon={<IconLayerText16 />}
+                value={textLayer.name}
+                onChange={(e) => handleInput(index, e)}
+              />
+              <Textbox
+                value={textLayer.value}
+                onChange={(e) => handleInput(index, e)}
+              />
+              <IconButton onClick={(e) => handleCreateComponent(index, e)}>
+                <IconComponent32 />
+              </IconButton>
+            </Columns>
+          </div>
+        );
+      })}
+      {/* </form> */}
+      <VerticalSpace space='small' />
+      <Columns space='extraSmall'>
+        <Button fullWidth onClick={handleCreateRectanglesButtonClick}>
+          Create
+        </Button>
+        <Button fullWidth onClick={handleCloseButtonClick} secondary>
+          Close
+        </Button>
+      </Columns>
+      <VerticalSpace space='small' />
+    </Container>
+  );
+};
 
-export { Layers }
+export { Layers };
