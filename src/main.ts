34d317900833a65265550a8674825b0dc62ab7dc
Apirak Panatkool <apirakb@gmail.com>
2023-01-02 08:31:15 +0700

    Use on and emit to send receive message between ui and main

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { once, showUI } from "@create-figma-plugin/utilities";
+import { emit, once, on, showUI } from "@create-figma-plugin/utilities";
 import { CloseHandler } from "./types";
 import {
   getLanguageArray,
@@ -7,17 +7,22 @@ import {
 import { updateAll } from "./updateText";
 import { i18nInit } from "./utility/i18nUtility";
 
-const updateLanguageToUI = () => {
-  const languages = {
-    languages: getLanguageArray(),
-  };
-  console.log("Send message to UI:", languages);
-  figma.ui.postMessage(languages);
-};
-
 const initUIPlugin = async () => {
   await initLanguageStorage();
   await i18nInit();
+};
+
+// Post message to UI
+const updateLanguageToUI = () => {
+  console.log("Send message to UI:", getLanguageArray());
+
+  // Original
+  // const languageMessage = {
+  //   languages: getLanguageArray(),
+  // };
+  // figma.ui.postMessage(languagesMessage);
+
+  emit("UPDATE_LANGUAGES", getLanguageArray());
 };
 
 export default function () {
@@ -31,8 +36,18 @@ export default function () {
     height: 500,
   });
 
-  figma.ui.onmessage = (message, payload: any) => {
-    console.log("Plugin got Message:", message);
+  // Receive message from UI
+
+  // Original
+  // figma.ui.onmessage = (message, payload: any) => {
+  //   console.log("Recieve message from UI:", message);
+  //   updateLanguageToUI();
+  // };
+
+  // New toy
+  const handleChageTab2 = (data: any) => {
+    console.log("Receive message from UI", data);
     updateLanguageToUI();
   };
+  on("CHANGE_TAB_2", updateLanguageToUI);
 }
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,10 +1,11 @@
-import { emit } from "@create-figma-plugin/utilities";
+import { emit, once, on } from "@create-figma-plugin/utilities";
 import { h, JSX } from "preact";
 import { useState } from "preact/hooks";
 import { Layers } from "./page/layers";
 import { Setting } from "./page/setting";
 import { Library } from "./page/Library";
 import { Components } from "./page/components";
+import { Languages } from "./utility/languageStorage";
 // import { CloseHandler, CreateRectanglesHandler } from "./types";
 import {
   Button,
@@ -23,13 +24,36 @@ function Plugin() {
   const [currentTab, setCurrentTab] = useState("Library");
   const [languages, setLanguages] = useState([]);
 
+  // Post message to main
   function handleChange(event: JSX.TargetedEvent<HTMLInputElement>) {
     const newValue = event.currentTarget.value;
     console.log("UI Tab Change:", newValue);
     setCurrentTab(newValue);
-    const type = "CHANGE_TAB";
-    parent.postMessage({ pluginMessage: { type, newValue } }, "*");
+
+    console.log("Post message to main:", newValue);
+
+    // Original
+    // const messageName = "CHANGE_TAB";
+    // parent.postMessage({ pluginMessage: { messageName, newValue } }, "*");
+
+    // New toy
+    emit("CHANGE_TAB_2", newValue);
   }
+
+  // Receive message from main
+
+  // Original
+  // onmessage = (event) => {
+  //   console.log("Receive message from main");
+  //   setLanguages(event.data.pluginMessage.languages);
+  //   console.log("Message:", languages);
+  // };
+
+  const handleUpdateLanguage = (languages: any) => {
+    console.log("Receive message from main", languages);
+    setLanguages(languages);
+  };
+  on("UPDATE_LANGUAGES", handleUpdateLanguage);
 
   const options: Array<TabsOption> = [
     {
@@ -49,12 +73,6 @@ function Plugin() {
       value: "Setting",
     },
   ];
-
-  onmessage = (event) => {
-    console.log("Receive message from Plugin");
-    setLanguages(event.data.pluginMessage.languages);
-    console.log("Message:", languages);
-  };
 
   return <Tabs onChange={handleChange} options={options} value={currentTab} />;
 }
